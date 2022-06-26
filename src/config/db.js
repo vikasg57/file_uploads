ac23e21a3a9374916877834843f5802b09ebd429
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-06-26 12:50:41 +0530

    connection for mongodb

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = async function connect() {
+  try {
+  
+    await mongoose.connect(process.env.URL);
+    
+    console.log("connected to database");
+  } catch (error) {
+    console.log(error);
+    console.log("could not connect to database");
+  }
+}
